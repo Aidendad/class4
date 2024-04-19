5ebe4f73ad338859d3bc42033086e6e5fbab0fe7
Aidendad Cho <justin.cho@kellogg.northwestern.edu>
2024-04-19 14:20:30 +0000

    aidendad_class4_2404190920

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -4,12 +4,59 @@
 
 # Booleans
 
+
 # Boolean Expressions
+value1 = 3
+value2 = 2
+# puts value1 == value2
+# puts value1 != value2
+# puts value1 > value2
+# puts value1 < value2
+# puts value1 >= value2
+
+
 
 # If Conditional Logic
+# if condition
+#     do something
+#     end
+
+if value1 == value2
+    puts "math is broken!"
+else 
+    puts "phew"
+end
+
+
 
 # If/Else Conditional Logic
+user_entere_password = "tacos"
+real_password = "secret"
+
+if user_entere_password == real_password
+    puts "great, you're logged in!"
+else
+    puts "wrong password."
+end
+
 
 # Elsif Conditional Logic
+home_team = 2
+away_team = 2
+if home_team > away_team
+    puts "we won!!!"
+#else if they're the same, we're tied
+elsif home_team == away_team
+    puts "ok, a tie"
+else
+    puts "we lost!"
+end
 
 # Combining Expressions
+temp = 70
+if temp >= 65 || temp <= 80
+    puts "it's nice!"
+else
+    puts "ughhh"
+end
+
